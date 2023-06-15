52563b8d69529d02c4f993eb7edd8055ab2a6cb6
4NDR0S <andross.vela@gmail.com>
2023-06-14 20:24:26 -0500

    try

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        Square cuadrado1 = new Square("rojo",4);
+        cuadrado1.GetColor();
+        cuadrado1.GetArea();
+
+        Console.WriteLine(cuadrado1.GetColor());
+        Console.WriteLine(cuadrado1.GetArea());
     }
 }
